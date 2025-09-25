318960ef54468ddd7ff70c8df43539dd01dc232f
Swapnaja27 <142212367+Swapnaja27@users.noreply.github.com>
2025-09-25 11:11:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function volume_sphere() {
+function volume_sphere(radius) {
     //Write your code here
-  
+    let result=(4/3)*Math.PI*radius*radius*radius;
+	return result;
 } 
+
+let radius=document.getElementById("radius");
+let volume=document.getElementById("volume");
+let btn=document.getElementById("submit");
+
+btn.addEventListener("click",()=>
+	{
+		if(radius<0){
+			volume.textContent="NAN";
+		}else{
+			volume_sphere(radius);
+		}
+	})
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
